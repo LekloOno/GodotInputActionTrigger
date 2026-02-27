61b6c32059e06b8a15be939f655dfcaa97e0b671
LekloOno <leklo.sounds@gmail.com>
2026-02-27 17:16:50 +0100

    Simple Press Input TriggerNode abstraction

---
+++ Nodes/Trigger/PressNodeTrigger.cs
@@ -0,0 +1,8 @@
+namespace GIAT.Nodes.Action;
+
+using GIAT.Nodes.Input.Type;
+using GIAT.Components.Trigger;
+using Godot;
+
+[GlobalClass, Tool]
+public abstract partial class PressNodeTrigger : NodeTrigger<PressInput>{}
