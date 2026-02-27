d95238923233d22aff654f11edaa9b05efbe7015
LekloOno <leklo.sounds@gmail.com>
2026-02-27 17:10:53 +0100

    Base trigger components

---
+++ Components/Trigger/FreeTrigger.cs
@@ -0,0 +1,8 @@
+namespace GIAT.Components.Trigger;
+
+using Godot;
+using GIAT.Interface;
+
+
+[GlobalClass]
+public abstract partial class FreeTrigger : Node, ITrigger {}
---
+++ Components/Trigger/NodeTrigger.cs
@@ -0,0 +1,75 @@
+namespace GIAT.Components.Trigger;
+
+using System.Collections.Generic;
+using Godot;
+
+using GIAT.Interface;
+
+[Tool]
+public abstract partial class NodeTrigger<T> : FreeTrigger where T: class, IInput
+{
+    protected List<IAction<T>> _actions = [];
+
+    private void CheckParent()
+    {
+        CheckParentSpec();
+
+        if (_actions.Count != 0)
+            return;
+
+        if (GetParent() is not IAction<T> action)
+            return;
+
+        if (action is NodeTrigger<T>)
+            return;
+
+        _actions = [action];
+    }
+
+    protected abstract void CheckParentSpec();
+
+    private void CheckChildren()
+    {
+        CheckChildrenSpec();
+        List<IAction<T>> actions = [];
+        foreach(Node node in GetChildren())
+            if (node is IAction<T> action)
+                actions.Add(action);
+        
+        // In the case where the trigger has a parent action,
+        // and a new unrelated node appears in children list.
+        // Prevent _actions from being emptied.
+        if (actions.Count != 0)
+            _actions = actions;
+    }
+
+    protected abstract void CheckChildrenSpec();
+
+    private void Unparent()
+    {
+        UnparentSpec();
+        _actions = [];
+    }
+
+    protected abstract void UnparentSpec();
+
+    public override void _Notification(int what)
+    {
+        switch ((long) what)
+        {
+            case NotificationParented:
+                CheckParent();
+                break;
+            case NotificationUnparented:
+                Unparent();
+                break;
+            case NotificationChildOrderChanged:
+                CheckChildren();
+                break;
+            default:
+                return;
+        }
+
+        UpdateConfigurationWarnings();
+    }
+}
